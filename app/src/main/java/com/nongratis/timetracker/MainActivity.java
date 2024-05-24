2e76f0c496d3d2b8ad20282f8ac44d1b28eb8775
Shapovalov Andrii <shapovalov.andrii@lll.kpi.ua>
2024-05-24 14:59:17 +0300

    first setup for ViewPager2

--- app/src/main/java/com/nongratis/timetracker/MainActivity.java
+++ app/src/main/java/com/nongratis/timetracker/MainActivity.java
@@ -1,4 +1,21 @@
 package com.nongratis.timetracker;
 
-public class MainActivity {
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
+import com.nongratis.timetracker.adapter.ViewPagerAdapter;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ViewPager2 viewPager2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        viewPager2 = findViewById(R.id.viewPager);
+        viewPager2.setAdapter(new ViewPagerAdapter(this));
+
+    }
 }
---
+++ app/src/main/java/com/nongratis/timetracker/adapter/ViewPagerAdapter.java
@@ -0,0 +1,36 @@
+package com.nongratis.timetracker.adapter;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import com.nongratis.timetracker.fragments.TimerFragment;
+import com.nongratis.timetracker.fragments.DailyTimelineFragment;
+import com.nongratis.timetracker.fragments.TimeAnalysisFragment;
+
+public class ViewPagerAdapter extends FragmentStateAdapter {
+
+    public ViewPagerAdapter(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position) {
+            case 0:
+                return new TimerFragment();
+            case 1:
+                return new DailyTimelineFragment();
+            case 2:
+                return new TimeAnalysisFragment();
+            default:
+                return new TimerFragment();
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return 3;
+    }
+}
