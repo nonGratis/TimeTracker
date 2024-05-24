b2da0a99a9c816ef6d3dd51b782bbe7d00a277eb
Shapovalov Andrii <shapovalov.andrii@lll.kpi.ua>
2024-05-24 17:56:46 +0300

    init setup of the frontend dropdown menu

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,6 +64,7 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.room.common)
     implementation(libs.androidx.room.runtime)
+    implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
