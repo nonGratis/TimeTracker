1c8427b650389706a3f707000a559de463fdc930
Shapovalov Andrii <shapovalov.andrii@lll.kpi.ua>
2024-05-25 13:24:34 +0300

    remove unnecessary version checking

--- app/src/main/java/com/nongratis/timetracker/utils/NotificationHelper.java
+++ app/src/main/java/com/nongratis/timetracker/utils/NotificationHelper.java
@@ -6,7 +6,6 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
 
 import androidx.core.app.NotificationCompat;
 
@@ -26,11 +25,9 @@ public class NotificationHelper {
         this.context = context;
         this.notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT);
-            channel.setDescription(CHANNEL_DESC);
-            notificationManager.createNotificationChannel(channel);
-        }
+        NotificationChannel channel = new NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT);
+        channel.setDescription(CHANNEL_DESC);
+        notificationManager.createNotificationChannel(channel);
     }
 
     public void startNotification(String timerDuration, boolean isPaused) {
