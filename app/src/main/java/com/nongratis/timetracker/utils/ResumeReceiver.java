5ba497d6b4e6ba9a2188745567b8bb394e98ee64
Shapovalov Andrii <shapovalov.andrii@lll.kpi.ua>
2024-05-25 12:41:57 +0300

    remove unnecessary import statement in ResumeReceiver

--- app/src/main/java/com/nongratis/timetracker/utils/ResumeReceiver.java
+++ app/src/main/java/com/nongratis/timetracker/utils/ResumeReceiver.java
@@ -1,6 +1,5 @@
 package com.nongratis.timetracker.utils;
 
-import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
