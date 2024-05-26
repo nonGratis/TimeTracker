7e79a893eeab9b03a0008040ff07d172384e0413
Shapovalov Andrii <shapovalov.andrii@lll.kpi.ua>
2024-05-26 17:09:51 +0300

    initial setup of timeline logic

--- app/src/main/java/com/nongratis/timetracker/fragments/DailyTimelineFragment.java
+++ app/src/main/java/com/nongratis/timetracker/fragments/DailyTimelineFragment.java
@@ -4,40 +4,57 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
-
-import com.nongratis.timetracker.AppDatabaseInitializer;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import com.nongratis.timetracker.R;
-import com.nongratis.timetracker.data.dao.TaskDao;
-import com.nongratis.timetracker.data.repository.ITaskRepository;
-import com.nongratis.timetracker.data.repository.TaskRepository;
+import com.nongratis.timetracker.adapter.TimelineAdapter;
 import com.nongratis.timetracker.viewmodel.DailyTimelineViewModel;
 
 public class DailyTimelineFragment extends Fragment {
+
     private DailyTimelineViewModel viewModel;
+    private RecyclerView timelineRecyclerView;
+    private TimelineAdapter adapter;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_daily_timeline, container, false);
-
-        TaskDao taskDao = AppDatabaseInitializer.getDatabase().taskDao();
-        TaskRepository taskRepository = new TaskRepository(taskDao);
-        ViewModelProvider.Factory factory = new ViewModelProvider.Factory() {
-            @NonNull
-            @Override
-            public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
-                return (T) new DailyTimelineViewModel(taskRepository);
-            }
-        };
-        viewModel = new ViewModelProvider(this, factory).get(DailyTimelineViewModel.class);
-
-        // TODO: Observe the tasks from the ViewModel and update the UI
+        timelineRecyclerView = view.findViewById(R.id.timelineRecyclerView);
+        timelineRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        viewModel = new ViewModelProvider(this).get(DailyTimelineViewModel.class);
+
+        // Assuming dayStart and dayEnd are calculated for the current day
+        long dayStart = calculateDayStart();
+        long dayEnd = calculateDayEnd();
+
+        viewModel.getTimeSegmentsForDay(dayStart, dayEnd).observe(getViewLifecycleOwner(), segments -> {
+            if (adapter == null) {
+                adapter = new TimelineAdapter(segments);
+                timelineRecyclerView.setAdapter(adapter);
+            } else {
+                adapter.updateTimeSegments(segments);
+            }
+        });
+    }
+
+    private long calculateDayStart() {
+        // Placeholder for actual day start calculation logic
+        return 0;
+    }
+
+    private long calculateDayEnd() {
+        // Placeholder for actual day end calculation logic
+        return 0;
     }
 }
--- app/src/main/java/com/nongratis/timetracker/viewmodel/DailyTimelineViewModel.java
+++ app/src/main/java/com/nongratis/timetracker/viewmodel/DailyTimelineViewModel.java
@@ -1,21 +1,39 @@
 package com.nongratis.timetracker.viewmodel;
 
 import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Transformations;
 import androidx.lifecycle.ViewModel;
 
 import com.nongratis.timetracker.data.entities.Task;
+import com.nongratis.timetracker.data.entities.TimeSegment;
 import com.nongratis.timetracker.data.repository.ITaskRepository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class DailyTimelineViewModel extends ViewModel {
     private final ITaskRepository taskRepository;
+    private LiveData<List<TimeSegment>> timeSegments;
 
     public DailyTimelineViewModel(ITaskRepository taskRepository) {
         this.taskRepository = taskRepository;
+        // Initialize timeSegments LiveData
+        timeSegments = new MutableLiveData<>();
     }
 
-    public LiveData<List<Task>> getTasksForDay(long dayStart, long dayEnd) {
-        return taskRepository.getTasksBetween(dayStart, dayEnd);
+    public LiveData<List<TimeSegment>> getTimeSegmentsForDay(long dayStart, long dayEnd) {
+        // Get tasks for the day and convert to time segments
+        LiveData<List<Task>> tasksForDay = taskRepository.getTasksBetween(dayStart, dayEnd);
+        timeSegments = Transformations.map(tasksForDay, this::convertTasksToTimeSegments);
+        return timeSegments;
+    }
+
+    private List<TimeSegment> convertTasksToTimeSegments(List<Task> tasks) {
+        List<TimeSegment> segments = new ArrayList<>();
+        for (Task task : tasks) {
+            segments.add(new TimeSegment(task.getStartTime(), task.getEndTime()));
+        }
+        return segments;
     }
 }
