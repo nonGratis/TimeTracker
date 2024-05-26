04aa3b648e01ea90de9fadf9bc8f34440172e87c
Shapovalov Andrii <shapovalov.andrii@lll.kpi.ua>
2024-05-26 14:12:55 +0300

    add Javadoc for TaskDao

--- app/src/main/java/com/nongratis/timetracker/data/dao/TaskDao.java
+++ app/src/main/java/com/nongratis/timetracker/data/dao/TaskDao.java
@@ -8,11 +8,26 @@ import com.nongratis.timetracker.data.entities.Task;
 
 import java.util.List;
 
+/**
+ * TaskDao is an interface that serves as the DAO (Data Access Object) for the Task entity.
+ * It provides methods to perform database operations on the tasks table.
+ * It is a part of the Room Persistence Library which provides an abstraction layer over SQLite.
+ */
 @Dao
 public interface TaskDao {
+    /**
+     * TaskDao is an interface that serves as the DAO (Data Access Object) for the Task entity.
+     * It provides methods to perform database operations on the tasks table.
+     * It is a part of the Room Persistence Library which provides an abstraction layer over SQLite.
+     */
     @Insert
     void insert(Task task);
 
+    /**
+     * Retrieves all tasks from the tasks table.
+     *
+     * @return A list of all tasks in the tasks table.
+     */
     @Query("SELECT * FROM tasks")
     List<Task> getAllTasks();
 }
