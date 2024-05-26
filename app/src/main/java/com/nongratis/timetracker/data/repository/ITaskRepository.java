e31f9c0576e7d30644c64c6cf2751fd502c1de9a
Shapovalov Andrii <shapovalov.andrii@lll.kpi.ua>
2024-05-26 14:58:53 +0300

    add dependence between Timeline and DB repository

--- app/src/main/java/com/nongratis/timetracker/data/repository/ITaskRepository.java
+++ app/src/main/java/com/nongratis/timetracker/data/repository/ITaskRepository.java
@@ -1,7 +1,13 @@
 package com.nongratis.timetracker.data.repository;
 
+import androidx.lifecycle.LiveData;
+
 import com.nongratis.timetracker.data.entities.Task;
+
+import java.util.List;
 
 public interface ITaskRepository {
     void insertTask(Task task) throws Exception;
+
+    LiveData<List<Task>> getTasksBetween(long dayStart, long dayEnd);
 }
--- app/src/main/java/com/nongratis/timetracker/data/repository/TaskRepository.java
+++ app/src/main/java/com/nongratis/timetracker/data/repository/TaskRepository.java
@@ -1,8 +1,12 @@
 package com.nongratis.timetracker.data.repository;
 
+import androidx.lifecycle.LiveData;
+
 import com.nongratis.timetracker.data.dao.TaskDao;
 import com.nongratis.timetracker.data.entities.Task;
 import com.nongratis.timetracker.data.executor.DatabaseExecutor;
+
+import java.util.List;
 
 public class TaskRepository implements ITaskRepository {
     private final TaskDao taskDao;
@@ -20,5 +24,11 @@ public class TaskRepository implements ITaskRepository {
         } catch (Exception e) {
             throw new Exception("Error inserting task", e);
         }
+    }
+
+    @Override
+    public LiveData<List<Task>> getTasksBetween(long dayStart, long dayEnd) {
+        // TODO: Implement the method to fetch tasks within the specified time range
+        return null;
     }
 }
