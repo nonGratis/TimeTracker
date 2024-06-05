4104021d7c42e46130b37458a4d40bc7f2eaed8b
Shapovalov Andrii <shapovalov.andrii@lll.kpi.ua>
2024-06-05 16:07:05 +0300

    update TimeAnalysisDao

--- app/src/main/java/com/nongratis/timetracker/data/dao/TimeAnalysisDao.java
+++ app/src/main/java/com/nongratis/timetracker/data/dao/TimeAnalysisDao.java
@@ -1,4 +1,16 @@
 package com.nongratis.timetracker.data.dao;
 
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Query;
+
+import com.nongratis.timetracker.data.entities.TimeAnalysisEntity;
+
+import java.util.List;
+
+@Dao
 public interface TimeAnalysisDao {
+    @Query("SELECT * FROM time_analysis WHERE timestamp >= :from AND timestamp <= :to")
+    LiveData<List<TimeAnalysisEntity>> getTimeAnalysis(long from, long to);
+    // Additional queries for aggregation
 }
