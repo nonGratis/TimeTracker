749c0484e84d176f179d87bc72838c1e161d4a6e
Shapovalov Andrii <shapovalov.andrii@lll.kpi.ua>
2024-06-05 21:13:57 +0300

    add diff color and other category group

--- app/src/main/java/com/nongratis/timetracker/fragments/TimeAnalysisFragment.java
+++ app/src/main/java/com/nongratis/timetracker/fragments/TimeAnalysisFragment.java
@@ -1,5 +1,6 @@
 package com.nongratis.timetracker.fragments;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,10 +13,12 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
 import com.nongratis.timetracker.R;
+import com.nongratis.timetracker.data.entities.Task;
 import com.nongratis.timetracker.data.repository.TaskRepository;
 import com.nongratis.timetracker.viewmodel.TaskViewModel;
 import com.nongratis.timetracker.viewmodel.TaskViewModelFactory;
@@ -64,15 +67,37 @@ public class TimeAnalysisFragment extends Fragment {
         List<PieEntry> entries = new ArrayList<>();
         // Process tasks to create PieEntries based on type (workflow/project)
         for (Task task : tasks) {
+            String label;
             if (type.equals("workflow")) {
-                entries.add(new PieEntry(task.getDuration(), task.getWorkflowName()));
+                label = task.getWorkflowName();
             } else {
-                entries.add(new PieEntry(task.getDuration(), task.getProjectName()));
+                label = task.getProjectName();
             }
+            if (label == null || label.isEmpty()) {
+                label = "Other";
+            }
+            entries.add(new PieEntry(task.getDuration(), label));
         }
+
         PieDataSet dataSet = new PieDataSet(entries, "Time Analysis");
+
+        // Assign different colors for different categories
+        List<Integer> colors = new ArrayList<>();
+        for (int i = 0; i < entries.size(); i++) {
+            colors.add(Color.rgb((i * 40) % 255, (i * 70) % 255, (i * 100) % 255));
+        }
+        dataSet.setColors(colors);
+
         PieData data = new PieData(dataSet);
         pieChart.setData(data);
         pieChart.invalidate(); // Refresh the chart
+
+        // Setup legend
+        Legend legend = pieChart.getLegend();
+        legend.setEnabled(true);
+        legend.setVerticalAlignment(Legend.LegendVerticalAlignment.BOTTOM);
+        legend.setHorizontalAlignment(Legend.LegendHorizontalAlignment.CENTER);
+        legend.setOrientation(Legend.LegendOrientation.HORIZONTAL);
+        legend.setDrawInside(false);
     }
 }
