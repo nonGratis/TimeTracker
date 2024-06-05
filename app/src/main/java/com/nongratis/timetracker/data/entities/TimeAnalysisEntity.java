2f3a97f13b529e5c7281f996212c7148dff223ae
Shapovalov Andrii <shapovalov.andrii@lll.kpi.ua>
2024-06-05 16:06:56 +0300

    update TimeAnalysisEntity

--- app/src/main/java/com/nongratis/timetracker/data/entities/TimeAnalysisEntity.java
+++ app/src/main/java/com/nongratis/timetracker/data/entities/TimeAnalysisEntity.java
@@ -1,4 +1,15 @@
 package com.nongratis.timetracker.data.entities;
 
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "time_analysis")
 public class TimeAnalysisEntity {
+    @PrimaryKey(autoGenerate = true)
+    public int id;
+    public String projectName;
+    public String workflowCategory;
+    public long duration;
+    public long timestamp;
+    // Getters and Setters
 }
