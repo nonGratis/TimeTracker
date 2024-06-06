fc25d1aca56277cc1da62f035e2c4e8566a93d4b
Shapovalov Andrii <shapovalov.andrii@lll.kpi.ua>
2024-06-06 05:10:21 +0300

    setup 2 tabs icon to button

--- app/src/main/java/com/nongratis/timetracker/activities/MainActivity.java
+++ app/src/main/java/com/nongratis/timetracker/activities/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager2.widget.ViewPager2;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 import com.nongratis.timetracker.R;
 import com.nongratis.timetracker.adapter.ViewPagerAdapter;
@@ -11,6 +13,7 @@ import com.nongratis.timetracker.adapter.ViewPagerAdapter;
 public class MainActivity extends AppCompatActivity {
 
     private ViewPager2 viewPager2;
+    private TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +21,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         viewPager2 = findViewById(R.id.viewPager);
+        tabLayout = findViewById(R.id.tab_layout);
+
         viewPager2.setAdapter(new ViewPagerAdapter(this));
-        viewPager2.setCurrentItem(0);
+
+        new TabLayoutMediator(tabLayout, viewPager2, (tab, position) -> {
+            switch (position) {
+                case 0:
+                    tab.setText("Timer");
+                    tab.setIcon(R.drawable.ic_timer); // replace with your timer icon
+                    break;
+                case 1:
+                    tab.setText("Time Analysis");
+                    tab.setIcon(R.drawable.ic_analysis); // replace with your analysis icon
+                    break;
+            }
+        }).attach();
     }
 }
