9ba9f29a1431bb3ea0deb186053e558d5436613d
Shapovalov Andrii <shapovalov.andrii@lll.kpi.ua>
2024-06-07 18:02:58 +0300

    delete Constants.java

--- app/src/main/java/com/nongratis/timetracker/Constants.java
+++
@@ -1,8 +0,0 @@
-package com.nongratis.timetracker;
-
-public class Constants {
-    public static final String ACTION_PAUSE_TIMER = "PAUSE_TIMER";
-    public static final String ACTION_STOP_TIMER = "STOP_TIMER";
-    public static final String ACTION_RESUME_TIMER = "RESUME_TIMER";
-    public static final int NOTIFICATION_DELAY = 1000;
-}
