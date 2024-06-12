f9f1055a9af830d5e332d8aab5006f01674714d6
Shapovalov Andrii <shapovalov.andrii@lll.kpi.ua>
2024-06-12 14:58:10 +0300

    add TimerLogicTest

---
+++ app/src/test/java/com/nongratis/timetracker/utils/TimerLogicTest.java
@@ -0,0 +1,38 @@
+package com.nongratis.timetracker.utils;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TimerLogicTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void startTimer() {
+    }
+
+    @Test
+    public void stopTimer() {
+    }
+
+    @Test
+    public void pauseTimer() {
+    }
+
+    @Test
+    public void isRunning() {
+    }
+
+    @Test
+    public void isPaused() {
+    }
+}
