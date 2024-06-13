1ec89fb10ecdad16f54cbbf63389803339ad3bb6
Shapovalov Andrii <shapovalov.andrii@lll.kpi.ua>
2024-06-13 09:57:24 +0300

    update TimeUtils

--- app/src/main/java/com/nongratis/timetracker/utils/TimeUtils.java
+++ app/src/main/java/com/nongratis/timetracker/utils/TimeUtils.java
@@ -17,12 +17,22 @@ public class TimeUtils {
                 break;
             case "week":
                 calendar.set(Calendar.DAY_OF_WEEK, calendar.getFirstDayOfWeek());
+                calendar.set(Calendar.HOUR_OF_DAY, 0);
+                calendar.set(Calendar.MINUTE, 0);
+                calendar.set(Calendar.SECOND, 0);
+                calendar.set(Calendar.MILLISECOND, 0);
                 startTime = calendar.getTimeInMillis();
                 break;
             case "month":
                 calendar.set(Calendar.DAY_OF_MONTH, 1);
+                calendar.set(Calendar.HOUR_OF_DAY, 0);
+                calendar.set(Calendar.MINUTE, 0);
+                calendar.set(Calendar.SECOND, 0);
+                calendar.set(Calendar.MILLISECOND, 0);
                 startTime = calendar.getTimeInMillis();
                 break;
+            default:
+                throw new IllegalArgumentException("Invalid period: " + period);
         }
 
         return new long[]{startTime, endTime};
