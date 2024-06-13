1d980fef42e1f7cf9c84128da578b49540d05fb5
Shapovalov Andrii <shapovalov.andrii@lll.kpi.ua>
2024-06-13 09:03:31 +0300

    move period calculation logic to TimeUtils

--- app/src/main/java/com/nongratis/timetracker/data/repository/TaskRepository.java
+++ app/src/main/java/com/nongratis/timetracker/data/repository/TaskRepository.java
@@ -7,8 +7,8 @@ import androidx.lifecycle.LiveData;
 import com.nongratis.timetracker.data.dao.TaskDao;
 import com.nongratis.timetracker.data.database.AppDatabase;
 import com.nongratis.timetracker.data.entities.Task;
+import com.nongratis.timetracker.utils.TimeUtils;
 
-import java.util.Calendar;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -23,44 +23,29 @@ public class TaskRepository implements ITaskRepository {
         executorService = Executors.newSingleThreadExecutor();
     }
 
+    @Override
     public void insertTask(Task task) {
         executorService.execute(() -> taskDao.insert(task));
     }
 
+    @Override
     public void updateTask(Task task) {
         executorService.execute(() -> taskDao.update(task));
     }
 
+    @Override
     public void deleteTask(Task task) {
         executorService.execute(() -> taskDao.delete(task));
     }
 
+    @Override
     public LiveData<List<Task>> getAllTasks() {
         return taskDao.getAllTasks();
     }
 
+    @Override
     public LiveData<List<Task>> getTasksByPeriod(String period) {
-        long startTime = 0, endTime = System.currentTimeMillis();
-        Calendar calendar = Calendar.getInstance();
-
-        switch (period) {
-            case "day":
-                calendar.set(Calendar.HOUR_OF_DAY, 0);
-                calendar.set(Calendar.MINUTE, 0);
-                calendar.set(Calendar.SECOND, 0);
-                calendar.set(Calendar.MILLISECOND, 0);
-                startTime = calendar.getTimeInMillis();
-                break;
-            case "week":
-                calendar.set(Calendar.DAY_OF_WEEK, calendar.getFirstDayOfWeek());
-                startTime = calendar.getTimeInMillis();
-                break;
-            case "month":
-                calendar.set(Calendar.DAY_OF_MONTH, 1);
-                startTime = calendar.getTimeInMillis();
-                break;
-        }
-
-        return taskDao.getTasksByPeriod(startTime, endTime);
+        long[] timeRange = TimeUtils.getTimeRangeForPeriod(period);
+        return taskDao.getTasksByPeriod(timeRange[0], timeRange[1]);
     }
 }
